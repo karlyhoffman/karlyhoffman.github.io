4fee03521f861c2886ee338ac423d69c6c0e54c1
Karly <Karly@HungryHoffKitchen.local>
2016-12-02 18:50:27 -0600

    Portfolio re-design

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,35 @@
-window.onload = function(event) {
-    console.log("Hey, everything has loaded.");
-    var age = 2016 - 1989;
-    console.log(age);
-};
+$(function() {
+	console.log("Linked.");
+
+	var paddingHeight = $(window).height() * .035 ;
+	console.log(paddingHeight);
+	var landingHeight = paddingHeight + $("#landing").height() + $("#bar").height() + $("h2").height() + $("nav").height() + $("label").height(); 
+
+
+	console.log(landingHeight);
+
+	var paddingHeight = $(window).height();
+
+	$(window).scroll(function() {
+	    var height = $(window).scrollTop();
+	    var nav = $('nav');
+
+	    if( height >= landingHeight) {
+			console.log("Hit the nav!");
+			nav.addClass('fix-nav')
+	    } else {
+	    	console.log("Haven't hit the nav, yet.");
+	    	nav.removeClass('fix-nav')
+	    }
+	});
+
+	function scrollNav() {
+		$('#bar, #projects').click(function(){  
+	    //Animate
+		    $('html, body').animate({
+			    scrollTop: $("#projects").offset().top
+			}, 1000);
+		});
+	};
+	scrollNav();
+});
