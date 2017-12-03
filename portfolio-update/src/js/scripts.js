aadd0211e271d78b14f03c3408098ddac8bc1d12
Karly <karlyhoffman@gmail.com>
2017-12-02 21:51:14 -0600

    All pages — scroll-to feature added

--- portfolio-update/src/js/scripts.js
+++ portfolio-update/src/js/scripts.js
@@ -36,6 +36,18 @@
       }
     }
 
+    // Scroll to href
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if(target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 100
+            }, 1000);
+        }
+
+    });
+
     // Reveal animations
     var controller = new ScrollMagic.Controller();
 
